036ee3f56a455e18020994f26ad7fb92adfed4b2
Astle Dsa <astledsa@gmail.com>
2024-10-03 05:13:58 +0530

    fix api problem

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import threading
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-from src.api import (
+from api import (
     get_matches_for_doc,
     get_matches_for_phrase,
     get_matches_for_url,
