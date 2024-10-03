cad502a0d5db68b5273c5ddfab056ecda388c97b
Astle Dsa <astledsa@gmail.com>
2024-10-03 23:06:51 +0530

    import error fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import threading
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-from src.api import (
+from api import (
     getOpposite,
     getForDocument,
     getForPhrase,
